2c2c0a577a805eb78d92cae11cb8a150cafbb8b7
nishantHolla <nishantholla19@gmail.com>
2023-08-03 00:06:11 +0530

    Add snake head

--- source/include/snake.hpp
+++ source/include/snake.hpp
@@ -7,6 +7,9 @@
 #include <SDL2/SDL.h>
 #include "sisIO.hpp"
 
+#define FPS 10
+#define FRAME_TARGET 1000.0/FPS
+
 #define WINDOW_TITLE "Snake"
 #define WINDOW_POS_X 0
 #define WINDOW_POS_Y 0
@@ -14,26 +17,54 @@
 #define WINDOW_HEIGHT 720
 #define WINDOW_FLAGS SDL_WINDOW_SHOWN
 
+#define CELL_WIDTH 40
+#define CELL_HEIGHT 40
+#define PADDING 10
+#define START_X 0
+#define START_Y CELL_HEIGHT
+
 #define SET_DRAW_COLOR(x, y) SDL_SetRenderDrawColor(x, y.r, y.g, y.b, y.a)
 
 class Snake {
 public:
 	Snake();
 	bool isRunning() {return running;};
+
+	void startFrame();
 	void pollEvents();
-	void updateScreen();
+	void update();
+	void show();
+	void endFrame();
+
 	~Snake();
 
 private:
+	struct Vec2 {
+	public:
+		int x;
+		int y;
+	};
+
 	SisIO io;
 
-	SDL_Window *window;
-	SDL_Renderer *renderer;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 
 	SDL_Color backgroundColor;
+	SDL_Color borderColor;
+	SDL_Color snakeColor;
 
 	bool running;
+	SDL_Rect snakeHead;
+	Vec2 direction;
+	Uint64 startTick;
+	Uint64 endTick;
 
+	void makeCell(SDL_Rect& _rect, int _x, int _y);
+	void showCell(SDL_Rect& _rect, SDL_Color& _color);
+
+// errors
+private:
 	class InitError : public std::exception {
 	public:
 		InitError(const char* _source) :
@@ -45,6 +76,8 @@ private:
 	private:
 		const char* source;
 	};
+
+
 };
 
 #endif //!SNAKE_H_
--- source/snake.cpp
+++ source/snake.cpp
@@ -4,7 +4,8 @@
 Snake::Snake() :
 	io ("./snakeLog"),
 	backgroundColor {0, 0, 0, 255},
-	running (true)
+	borderColor {255, 255, 255, 255},
+	snakeColor {255, 0, 0, 255}
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 		throw InitError("SDL");
@@ -17,22 +18,78 @@ Snake::Snake() :
 	if (!renderer)
 		throw InitError("SDL renderer");
 
+	running = true;
+	makeCell(snakeHead, START_X, START_Y);
+	direction = {1, 0};
+
 	io.output(SisIO::messageType::okay, "Game started successfully.");
+}
+
+void Snake::startFrame() {
+	startTick = SDL_GetPerformanceCounter();
 }
 
 void Snake::pollEvents() {
 	SDL_Event event = {0};
 	while (SDL_PollEvent(&event)) {
-		if (event.type == SDL_QUIT)
+		if (event.type == SDL_QUIT) {
 			running = false;
+			continue;
+		}
+
+		int key = -1;
+		if (event.type == SDL_KEYDOWN)
+			key = event.key.keysym.sym;
+
+		if (key == SDLK_DOWN)
+			direction = {0, 1};
+
+		else if (key == SDLK_RIGHT)
+			direction = {1, 0};
+
+		else if (key == SDLK_LEFT)
+			direction = {-1, 0};
+
+		else if (key == SDLK_UP)
+			direction = {0, -1};
 	}
 }
 
-void Snake::updateScreen() {
+void Snake::update() {
+	snakeHead.x += (CELL_WIDTH * direction.x);
+	snakeHead.y += (CELL_WIDTH * direction.y);
+}
+
+void Snake::show() {
 	SET_DRAW_COLOR(renderer, backgroundColor);
 	SDL_RenderClear(renderer);
 
+	SET_DRAW_COLOR(renderer, borderColor);
+	SDL_RenderDrawLine(renderer, 0, START_Y, WINDOW_WIDTH, START_Y);
+
+	showCell(snakeHead, snakeColor);
+
 	SDL_RenderPresent(renderer);
+}
+
+void Snake::endFrame() {
+	endTick = SDL_GetPerformanceCounter();
+
+	float elapsedMS = (endTick - startTick) / (float)SDL_GetPerformanceFrequency() * 1000.0f;
+	SDL_Delay(floor(FRAME_TARGET - elapsedMS));
+}
+
+void Snake::makeCell(SDL_Rect& _rect, int _x, int _y) {
+	_rect.x = _x + (PADDING/2);
+	_rect.y = _y + (PADDING/2);
+	_rect.w = CELL_WIDTH - PADDING;
+	_rect.h = CELL_HEIGHT - PADDING;
+}
+
+void Snake::showCell(SDL_Rect& _rect, SDL_Color& _color) {
+	SET_DRAW_COLOR(renderer, _color);
+	SDL_RenderFillRect(renderer, &_rect);
+	SDL_RenderDrawRect(renderer, &_rect);
 }
 
 Snake::~Snake() {
